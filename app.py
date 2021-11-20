958308408ff95071233091a71122f2accb336318
LingLv-git <55591800+LingLv-git@users.noreply.github.com>
2021-11-20 07:42:19 -0500

    added flask to model connection

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, jsonify
+from joblib import load
+from pandas import DataFrame
 # Flask Setup
 app = Flask(__name__)
+# load model
+clf = load('random_forest_model_in_use.joblib')
 
 @app.route("/")
 def home():
@@ -11,5 +14,20 @@ def home():
 def fortune():
     return render_template("eda.html")
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    #For rendering results on HTML GUI
+    payload = request.json
+    try:
+        input = DataFrame(data = {
+            "smoker_yes": [int(payload["smoke_yes"])],
+            "bmi": [float(payload["bmi"])],
+            "age": [int(payload["age"])]
+        })
+    except:
+        return jsonify({"cost": 0, "error": "invalid payload"})
+    cost = clf.predict(input)
+    return jsonify({"cost": cost[0]})
+        
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/bmi.js
+++ static/js/bmi.js
@@ -1,4 +1,5 @@
-d3.selectAll("button").on("click", function() {
+d3.selectAll("button").on("click", function () {
+    // error handling
 
     var age = d3.select("#age").property("value");
     var weight = d3.select("#weight").property("value");
@@ -7,7 +8,36 @@ d3.selectAll("button").on("click", function() {
     var smoke = d3.select('input[name="smoke"]:checked').node().value
 
     var height = (feet * 12 + inch);
-    var bmi = (weight / (height*height)) * 70300
+    var bmi = (weight / (height * height)) * 70300
     console.log(age, weight, feet, inch, smoke, bmi);
+    if (!weight) {
+        return alert('Weight can not be empty')
 
+    }
+    if (!age) {
+        return alert('Age can not be empty')
+    }
+    var data = {
+        age: age,
+        bmi,
+        smoke_yes: smoke
+    }
+    console.log(data)
+
+    fetch('/predict', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(d => {
+        console.log('response', d)
+        var cost = d.cost
+        // d3.select("#output").property("value", cost);
+        document.getElementById("output").innerHTML = "Cost: " + cost.toFixed(0)
+
+    })
 });
+
