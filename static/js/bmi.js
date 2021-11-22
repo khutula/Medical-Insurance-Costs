bafef9da3176030db6350e2280012dae1861ebac
Karina Hutula <khutula@g.clemson.edu>
2021-11-21 22:58:57 -0500

    remove console logs

--- static/js/bmi.js
+++ static/js/bmi.js
@@ -1,28 +1,29 @@
 d3.selectAll("button").on("click", function () {
-    // error handling
-
+    
     var age = d3.select("#age").property("value");
     var weight = d3.select("#weight").property("value");
     var feet = d3.select("#feet").property("value");
     var inch = d3.select("#inch").property("value");
-    var smoke = d3.select('input[name="smoke"]:checked').node().value
+    var smoke = d3.select('input[name="smoke"]:checked').node().value;
 
     var height = (parseFloat(feet * 12) + parseFloat(inch));
-    var bmi = (weight / (height * height)) * 703
+    var bmi = (weight / (height * height)) * 703;
     console.log(age, weight, feet, inch, height, smoke, bmi);
-    if (!weight) {
-        return alert('Weight can not be empty')
 
-    }
+    // error handling
+    if (!weight) {
+        return alert('Weight can not be empty');
+    };
+
     if (!age) {
-        return alert('Age can not be empty')
-    }
+        return alert('Age can not be empty');
+    };
+
     var data = {
         age: age,
         bmi: bmi,
         smoke_yes: smoke
-    }
-    console.log(data)
+    };
 
     fetch('/predict', {
         method: 'POST',
@@ -38,5 +39,5 @@ d3.selectAll("button").on("click", function () {
         // d3.select("#output").property("value", cost);
         document.getElementById("output").innerHTML = "Cost: $" + cost.toFixed(0)
 
-    })
+    });
 });
