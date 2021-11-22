b5ff9230a1a9579239990c43ca8e84d113c0d9dd
kyleshim <83684076+kyleshim@users.noreply.github.com>
2021-11-22 16:25:43 -0500

    Updated with Functional Plotly

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,22 +1,65 @@
 const url = "static/Resources/insurance.csv";
 
-// Fetch the JSON data and console log it
-d3.csv(url).then(function(data) {
-  var list = [];
-  for (var i=0; i<data.length; i++) {
-    list[i] = parseFloat(data[i].charges);
-  };
+d3.selectAll("button").on("click", function () {
+    // error handling
+    // Fetch the JSON data and console log it
+    d3.csv(url).then(function(datas) {
+      let list = [];
+      for (var i=0; i<datas.length; i++) {
+        list[i] = parseFloat(datas[i].charges);
+      };
+  
+    var age = d3.select("#age").property("value");
+    var weight = d3.select("#weight").property("value");
+    var feet = d3.select("#feet").property("value");
+    var inch = d3.select("#inch").property("value");
+    var smoke = d3.select('input[name="smoke"]:checked').node().value
 
-  var highlight = [];
-  for (var i=0; i<200; i++) {
-    highlight[i] = 12500; 
-  };
+    var height = (parseFloat(feet * 12) + parseFloat(inch));
+    var bmi = (weight / (height * height)) * 703
+    
+    console.log(age, weight, feet, inch, height, smoke, bmi);
+    if (!weight) {
+        return alert('Weight can not be empty')
 
-  CreateChart(list, highlight);
+    }
+    if (!age) {
+        return alert('Age can not be empty')
+    }
+    var data = {
+        age: age,
+        bmi: bmi,
+        smoke_yes: smoke
+    }
+    console.log(data)
+
+    
+    fetch('/predict', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+    .then(d => {
+        console.log('response', d)
+        var cost = d.cost
+        // d3.select("#output").property("value", cost);
+        document.getElementById("output").innerHTML = "Cost: $" + cost.toFixed(0);
+        CreateChart(list, cost);
+        });
+    });
 });
- 
-function CreateChart(list, highlight) {
+  
+function CreateChart(list, cost) {
 
+//    let cost = d3.select("#output").property("value", cost);
+    let highlight = [];
+        for (var i=0; i<200; i++) {
+            highlight[i] = cost;   
+        };
+    
   var trace1 = {
     x: list,
     type: "histogram",
@@ -27,7 +70,7 @@ function CreateChart(list, highlight) {
   var trace2 = {
     x: highlight,
     type: "histogram",
-    opacity: 0.5,
+    opacity: 0.6,
     name: 'You',
   };
 
@@ -36,7 +79,7 @@ function CreateChart(list, highlight) {
     barmode: "overlay",
     paper_bgcolor: '#DCD4D2',
     plot_bgcolor: '#DCD4D2',
-    colorway: ['#A7E0D7','#E65401','#FEF200'],
+    colorway: ['#0C1F38','#E65401','#FEF200'],
     font: {
       color: '#0C1F38'
     },
@@ -45,7 +88,20 @@ function CreateChart(list, highlight) {
       bordercolor: '#0C1F38',
       borderwidth: 2
     },
+    title: {
+      text: 'Distribution of Charges'
+    },
+    xaxis: {
+        title: {
+            text: 'Charges'
+        },
+    },
+    yaxis: {
+        title: {
+            text: 'Density'
+        },
+    },
 };
-
-  Plotly.newPlot("plot", hist, layout, {responsive: true});
+    
+  Plotly.newPlot("percentile", hist, layout, {responsive: true});
 };
